bccf9f9a97002e1a544637adf9840dc6bd648f7e
Davi Moreira <davimoreira58@gmail.com>
2020-04-18 01:08:05 -0300

    exc 075 realizado por Davi, imcompleto

---
+++ exc075.py
@@ -0,0 +1,17 @@
+# Desenvolva um programa que leia quatro valores pelo teclado e guadeos em uma tupla. Nofonal, mostre:
+# A) Quantas vezes o valor 9
+# B) Em que posição foi digitado o primeiro valor 3
+# C) Quais foram os numeros pares.
+
+n0 = int(input('Digite um valor: '))
+n1 = int(input('Digite outro valor: '))
+n2 = int(input('Digite mais um valor: '))
+n3 = int(input('Digite o ultimo valor: '))
+tupla = (n0, n1, n2, n3)
+print(f'O valor 9 apareceu {tupla.count(9)} vezes')
+if 3 in tupla:
+    print(f'O valor 3 apareceu na {tupla.index(3)}ª posição')
+else:
+    print('O valor 3 não apareceu')
+
+
