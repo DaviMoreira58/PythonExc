154ba10bc8f6fd0997ebc942a680d47012ffe911
Davi Moreira <davimoreira58@gmail.com>
2020-04-17 21:15:34 -0300

    exc 073 realizado por Davi

---
+++ exc073.py
@@ -0,0 +1,24 @@
+# Crie um tupla com os 20 primeiros colocados da tabela do campeonato brasileiro de
+# futebol, na ordem de colocação. Depois mostre:
+# A) Apenas os 5 primeiros colocados.
+# B) Os últimos 4 colocados na tabela.
+# C) Uma lista com os times em ordem alfabética
+# D) Em que posição na tabela está o time da Chapecoense
+
+tabela = ('Palmeiras', 'Flamengo', 'Internacional', 'Grêmio', 'São Paulo',
+          'Atlético - MG', 'Athletico-PR', 'Cruzeiro', 'Botafogo', 'Santos',
+          'Bahia', 'Fluminense', 'Corinthians', 'Chapecoense', 'Ceará', 'Vasco',
+          'Sport', 'América-MG', 'Vitória', 'Paraná Clube')
+print('-=' * 30)
+print(f'Lista de times do Brasileirão: {tabela}')
+print('-=' * 30)
+print(f'Os 5 primeiros: {tabela[:5]}')
+print('-=' * 30)
+print(f'Os 4 ultimos: {tabela[-4:]}')
+print('-=' * 30)
+print(f'Em ordem alfabética: {sorted(tabela)}')
+print('-=' * 30)
+pos = tabela.index('Chapecoense')
+print(f'A Chapecoense está na {pos}ª posição')
+print('-=' * 30)
+
