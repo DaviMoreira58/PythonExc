05ed63139b52f981ff3a011774737991f3bb1646
Davi Moreira <davimoreira58@gmail.com>
2020-04-20 00:33:17 -0300

    exc 077 jeito Guanabara

--- exc077.py
+++ exc077.py
@@ -2,11 +2,18 @@
 # Depois disso, você deve mostrar, para cada palavra, quais são suas vogais.
 # Na palavra APRENDER temos a e e
 
-palavras = ('aprender', 'programar', 'linguagem', 'python', 'curso', 'gratis',
+"""palavras = ('aprender', 'programar', 'linguagem', 'python', 'curso', 'gratis',
             'estudar', 'praticar', 'trabalhar', 'mercado', 'programardor', 'futuro')
 vogais = ('a', 'e', 'i', 'o', 'u')
 x = 0
 for cont in range(0, len(palavras)):
     print(f'{palavras[x]}')
-    x += 1
+    x += 1""" # Incompleto
 
+palavras = ('aprender', 'programar', 'linguagem', 'python', 'curso', 'gratis',
+            'estudar', 'praticar', 'trabalhar', 'mercado', 'programardor', 'futuro')
+for p in palavras:
+    print(f'\nNa palavra {p.upper()} temos ', end='')
+    for letra in p:
+        if letra.lower() in 'aeiou': # para acentos → 'aáàâãeèé...'
+            print(letra, end=' ')
