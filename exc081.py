3bce8bc2a1132a0b9d3379098f653dfccd9c5cfc
Davi Moreira <davimoreira58@gmail.com>
2020-04-21 15:10:01 -0300

    exc 081 jeito guanabara

--- exc081.py
+++ exc081.py
@@ -20,3 +20,18 @@ if 5 in lista:
     print('O valor 5 faz parte da lista!')
 else:
     print('O valor 5 não foi encontrado na lista!')""" # Meu jeito
+
+valores = []
+while True:
+    valores.append(int(input('Digite um valor: ')))
+    resp = str(input('Deseja continuar? [S/N] ')).upper().split()[0]
+    if resp in 'Nn':
+        break
+print('-=' * 30)
+print(f'Você digitou {len(valores)} elementos')
+valores.sort(reverse=True)
+print(f'Os valroes em ordem decrescente são {valores}')
+if 5 in valores:
+    print('O valor 5 faz parte da lista!')
+else:
+    print('O valor 5 não foi encontrado na lista!')
