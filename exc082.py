92ff5f31f76f37a354d99ac7b02a762f282c0c12
Davi Moreira <davimoreira58@gmail.com>
2020-04-21 15:37:13 -0300

    exc 082 meu jeito

---
+++ exc082.py
@@ -0,0 +1,21 @@
+# Crie um programa que vai ler vários números e colocar em uma lista. Depois disso,
+# crie duas listas extras que vão conter apenas os valores pares e os valores
+# ímpares digitados, respectivamente. Ao final, mostre o conteúdo das três listas
+# geradas.
+
+"""listaT = []
+listaP = []
+listaI = []
+while True:
+    n = int(input('Digite um numero: '))
+    listaT.append(n)
+    r = str(input('Deseja continuar? [S/N] ')).upper()
+    if r in 'Nn':
+        break
+    if n % 2 == 0:
+        listaP.append(n)
+    if n % 2 == 1:
+        listaI.append(n)
+print(f'A lista completa é {listaT}')
+print(f'A lista de pares é {listaP}')
+print(f'A lista de ímpares é {listaI}')"""
