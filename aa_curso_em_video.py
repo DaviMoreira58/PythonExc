519a182195772420110c7aecd0e1376e0b4749ad
Davi Moreira <davimoreira58@gmail.com>
2020-04-20 23:20:14 -0300

    exc078 do jeito guanabara

--- aa_curso_em_video.py
+++ aa_curso_em_video.py
@@ -127,7 +127,7 @@ B) Em que posição foi digitado o primeiro valor 3.
 C) Quais foram os números pares.
 Exercício Python 076: Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços, na sequência. No final, mostre uma listagem de preços, organizando os dados em forma tabular.
 Exercício Python 077: Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
-
+Exercício Python 078: Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
 
 
 
--- exc078.py
+++ exc078.py
@@ -3,7 +3,7 @@
 # respectivas posiçãoes na lista
 
 
-valores = list()
+"""valores = list()
 for cont in range(0, 5):
     valores.append(int(input(f'Digite o valor para a posição {cont}: ')))
 print(f'Você digitou os valores {valores}')
@@ -14,4 +14,28 @@ for c, v in enumerate(valores):
 print(f'\nO menor valor digitado foi {min(valores)} na posição ', end='')
 for c, v in enumerate(valores):
     if v == min(valores):
-        print(f'{c}... ', end='')
+        print(f'{c}... ', end='')""" # Meu jeito
+
+listanum = []
+mai = men = 0
+for c in range(0, 5):
+    listanum.append(int(input(f'Digite um valor para a posição {c} ')))
+    if c == 0:
+        mai = men = listanum[c]
+    else:
+        if listanum[c] > mai:
+            mai = listanum[c]
+        if listanum[c] < men:
+            men = listanum[c]
+print('-=' * 30)
+print(f'Você digitou os valores {listanum}')
+print(f'O maior valor digitado doi {mai} nas posições ', end='')
+for i, v in enumerate(listanum):
+    if v == mai:
+        print(f'{i}... ', end='')
+print()
+print(f'O menor valor digitado foi {men} nas posições ', end='')
+for i, v in enumerate(listanum):
+    if v == men:
+        print(f'{i}... ', end='')
+print()
