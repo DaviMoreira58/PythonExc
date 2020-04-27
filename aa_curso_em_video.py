2783c2de070529ff1153e3f4e2e5dc43771d1055
Davi Moreira <davimoreira58@gmail.com>
2020-04-27 19:00:12 -0300

    exc 086 davi

--- aa_curso_em_video.py
+++ aa_curso_em_video.py
@@ -141,7 +141,7 @@ A) Quantas pessoas foram cadastradas.
 B) Uma listagem com as pessoas mais pesadas.
 C) Uma listagem com as pessoas mais leves.
 Exercício Python 085: Crie um programa onde o usuário possa digitar sete valores numéricos e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente.
-
+Exercício Python 086: Crie um programa que declare uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado. No final, mostre a matriz na tela, com a formatação correta.
 
 
 
